e09304c9130888f1d2ae3d0b5be6b633c70f77aa
lepipele <lepipele@users.noreply.github.com>
2016-04-27 14:24:55 +0800

    Improving elmah logging

--- src/Elmah.Contrib.WebApi/ElmahExceptionLogger.cs
+++ src/Elmah.Contrib.WebApi/ElmahExceptionLogger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http.ExceptionHandling;
@@ -7,47 +8,54 @@ using System.Web.Http.ExceptionHandling;
 
 namespace Elmah.Contrib.WebApi
 {
-	public class ElmahExceptionLogger : ExceptionLogger
-	{
-		public override void Log(ExceptionLoggerContext context)
-		{
-			// Retrieve the current HttpContext instance for this request.
-			HttpContext httpContext = GetHttpContext(context.Request);
+    public class ElmahExceptionLogger : ExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            // Retrieve the current HttpContext instance for this request.
+            HttpContext httpContext = GetHttpContext(context.Request);
 
-			if (httpContext == null)
-				return;
+            if (httpContext == null)
+                return;
 
             // Wrap the exception in an HttpUnhandledException so that ELMAH can capture the original error page.
             string errorMessage = context.Exception.Message;
             try
             {
-                string data = context.Request.Content.ReadAsStringAsync().Result;
-                errorMessage += " " + data;
+                using (var stream = context.Request.Content.ReadAsStreamAsync().Result)
+                {
+                    stream.Seek(0, SeekOrigin.Begin);
+                    using (var reader = new StreamReader(stream))
+                    {
+                        string data = reader.ReadToEnd();
+                        errorMessage = data + "\n" + errorMessage;
+                    }
+                }
             }
             catch { }
             Exception exceptionToRaise = new HttpUnhandledException(errorMessage, context.Exception);
 
             // Send the exception to ELMAH (for logging, mailing, filtering, etc.).
             ErrorSignal signal = ErrorSignal.FromContext(httpContext);
-			signal.Raise(exceptionToRaise, httpContext);
-		}
+            signal.Raise(exceptionToRaise, httpContext);
+        }
 
-		private static HttpContext GetHttpContext(HttpRequestMessage request)
-		{
-			if (request == null)
-				return null;
+        private static HttpContext GetHttpContext(HttpRequestMessage request)
+        {
+            if (request == null)
+                return null;
 
-			object value;
-			if (request.Properties.TryGetValue("MS_HttpContext", out value))
-			{
-				HttpContextBase context = value as HttpContextBase;
-				if (context != null)
-					return context.ApplicationInstance.Context;
-			}
+            object value;
+            if (request.Properties.TryGetValue("MS_HttpContext", out value))
+            {
+                HttpContextBase context = value as HttpContextBase;
+                if (context != null)
+                    return context.ApplicationInstance.Context;
+            }
 
-			return HttpContext.Current;
-		}
-	}
+            return HttpContext.Current;
+        }
+    }
 }
 
 #pragma warning restore 1591
